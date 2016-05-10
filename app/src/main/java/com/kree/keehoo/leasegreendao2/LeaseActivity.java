23939c9f605917ee1a922f50052c00199b8388a9
Chris Kubicky <kkubicki2@gmail.com>
2016-05-10 15:59:31 +0200

    Fixed nullpointerexception for lease == null in the LeaseActivity,

--- app/src/main/java/com/kree/keehoo/leasegreendao2/LeaseActivity.java
+++ app/src/main/java/com/kree/keehoo/leasegreendao2/LeaseActivity.java
@@ -1,8 +1,8 @@
 package com.kree.keehoo.leasegreendao2;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -10,12 +10,13 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
-
 import com.kree.keehoo.leasegreendao2.model.DaoSession;
 import com.kree.keehoo.leasegreendao2.model.Lease;
 import com.kree.keehoo.leasegreendao2.model.LeaseDao;
+import com.kree.keehoo.leasegreendao2.model.Person;
+import com.kree.keehoo.leasegreendao2.model.PersonDao;
 
-public class LeaseActivity extends AppCompatActivity{
+public class LeaseActivity extends AppCompatActivity {
 
     public EditText editTextItem;
     public EditText editTextComment;
@@ -30,6 +31,8 @@ public class LeaseActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lease);
 
+        Log.d("LeaseActivity", "   onCreate inside LeaseActivity");
+
         editTextItem = (EditText) findViewById(R.id.activity_lease_item);
         editTextComment = (EditText) findViewById(R.id.activity_lease_comment);
         spinnerPerson = (Spinner) findViewById(R.id.activity_lease_person);
@@ -37,33 +40,56 @@ public class LeaseActivity extends AppCompatActivity{
         buttonDelete = (Button) findViewById(R.id.activity_lease_delete);
 
         Intent intent = getIntent();
-        Long leaseId = intent.getLongExtra("lease_id", 0);
-        if(leaseId != 0 ) {
+        Long leaseId = intent.getLongExtra("lease_id", 0L);
+        if (leaseId != 0) {
+            Log.d("LeaseActivity", "   leaseId != 0  --- leaseId to jest extra z intenta - lease_id ");
             DaoSession daoSession = ((LeaseApplication) getApplicationContext()).getDaoSession();
-
             LeaseDao leaseDao = daoSession.getLeaseDao();
             lease = leaseDao.load(leaseId);
-            if(lease != null) {
+            if (lease != null) {
                 editTextItem.setText(lease.getItem());
                 editTextComment.setText(lease.getComment());
-
+            } else {
+                lease = new Lease();
+                if (lease == null) {
+                    Log.d("LeaseActivity....", " lease nadal rowne jest null mimo wykonania lease = new Lease()");
+                    finish();
+                }
             }
         }
+
+        Log.d("LeaseActivity", "At this point we should have a clear information on what is lease ");
         buttonSave.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v)
-            {if(String.valueOf(editTextItem.getText())==null)
-                {
+            public void onClick(View v) {
+                if (String.valueOf(editTextItem.getText()) == null) {
                     Log.d("LeaseActivity", "text jest rowny null");
                     Toast.makeText(LeaseActivity.this, "String.valueOf(editTextItem.getText() == null !!!!!!!", Toast.LENGTH_LONG).show();
                 }
 
-                lease.setItem(String.valueOf(editTextItem.getText()));
-                lease.setComment(String.valueOf(editTextComment.getText()));
-                DaoSession daoSession = ((LeaseApplication) getApplicationContext()).getDaoSession();
-                daoSession.insertOrReplace(lease);
-                finish();
-            }
+
+                if (lease == null) {
+                    Log.d("---LeaseActivity---", " =====lease refernce is NULL !!!!!!!==== Why is this still null, bitch??");
+                    lease = new Lease();
+                }
+                    Log.d("LeaseActivity", "  OOOO   editTextItem value = " + editTextItem.getText().toString());
+                    lease.setItem(editTextItem.getText().toString());
+                    lease.setComment(editTextComment.getText().toString());
+                    DaoSession daoSession = ((LeaseApplication) getApplicationContext()).getDaoSession();
+
+                    // added in the fourth part of tutorial to randomize person, not select from spinner
+
+                    Person person = new Person();
+                    person.setName("John Doe " + Math.random());
+                    PersonDao personDao = daoSession.getPersonDao();
+                    personDao.insertOrReplace(person);
+
+                    lease.setPerson(person);
+
+                    daoSession.insertOrReplace(lease);
+                    finish();
+                }
+
         });
 
         buttonDelete.setOnClickListener(new View.OnClickListener() {
--- app/src/main/java/com/kree/keehoo/leasegreendao2/MainActivity.java
+++ app/src/main/java/com/kree/keehoo/leasegreendao2/MainActivity.java
@@ -1,12 +1,14 @@
 package com.kree.keehoo.leasegreendao2;
 
-import android.database.sqlite.SQLiteDatabase;
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
-import com.kree.keehoo.leasegreendao2.model.DaoMaster;
 import com.kree.keehoo.leasegreendao2.model.DaoSession;
 import com.kree.keehoo.leasegreendao2.model.Lease;
 import com.kree.keehoo.leasegreendao2.model.LeaseDao;
@@ -17,7 +19,7 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
+    /*@Override
     protected void onResume() {
         super.onResume();
         DaoSession daoSession = ((LeaseApplication) getApplicationContext()).getDaoSession();
@@ -29,28 +31,49 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         MainActivityListAdapter adapter = new MainActivityListAdapter(leaseList, this);
         recyclerView.setAdapter(adapter);
-    }
+    }*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        Log.d("MainActivity", "Main activity onCreate started");
         DaoSession daoSession = ((LeaseApplication) getApplicationContext()).getDaoSession();
         LeaseDao leaseDao = daoSession.getLeaseDao();
         List leaseList = leaseDao.loadAll();
-
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.main_activity_recyclerView);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         MainActivityListAdapter adapter = new MainActivityListAdapter(leaseList, this);
         recyclerView.setAdapter(adapter);
-
         insertSampleData(daoSession);
-
-
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_settings) {
+            Intent intent = new Intent(this, LeaseActivity.class);
+            intent.putExtra("lease_id", 0L);
+            startActivity(intent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+   /* @Override
+    public boolean onOptionsItemSelected(Menu item) {
+        getMenuInflater().inflate(R.menu.main_menu, item);
+        Intent intent = new Intent(this, LeaseActivity.class);
+        intent.putExtra("lease_id", 0);
+        this.startActivityForResult(intent, 1);
+
+        return true;
+    }*/
 
     public void insertSampleData(DaoSession daoSession) {
         Person person = new Person();
